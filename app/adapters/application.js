fdb53a6b37163b87cdd5d0e69461564076166669
stearagon <me@stevearagon.io>
2016-10-01 16:40:11 -0400

    Application Adapter: add namespace and host vlaues

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,11 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import ENV from 'bball-yall/config/environment';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:devise'
+    authorizer: 'authorizer:devise',
+
+    namespace: 'api',
+
+    host: ENV.APP.API_HOST,
 });
