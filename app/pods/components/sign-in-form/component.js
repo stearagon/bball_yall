a6051ce9818d3217730f11c06c0251b865303922
stearagon <me@stevearagon.io>
2016-12-18 02:08:10 -0500

    add spinner for login

--- app/pods/components/sign-in-form/component.js
+++ app/pods/components/sign-in-form/component.js
@@ -7,11 +7,16 @@ export default Ember.Component.extend({
 
   email: null,
   password: null,
+  isLoading: false,
 
   actions: {
     authenticate: function() {
+      this.set('isLoading', true);
       let { email, password } = this.getProperties('email', 'password');
-      return this.get('session').authenticate('authenticator:devise', email, password).catch((reason) => {
+      return this.get('session').authenticate('authenticator:devise', email, password).then(() => {
+        this.set('isLoading', false);
+      }, (reason) => {
+        this.set('isLoading', false);
         this.set('errorMessage', reason.error);
       });
     }
