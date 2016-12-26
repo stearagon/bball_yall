48e09766cebe76ad7a1bce7a3ed784f719128fc2
stearagon <me@stevearagon.io>
2016-12-26 14:39:40 -0500

    Add sign up form

---
+++ app/pods/sign-up/controller.js
@@ -0,0 +1,48 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  session: Ember.inject.service(),
+
+  email: null,
+  password: '',
+  passwordConfirm: '',
+  isLoading: false,
+  serverError: null,
+
+  passwordMatch: Ember.computed('password', 'passwordConfirm', function() {
+    const password = this.get('password');
+    const passwordConfirm = this.get('passwordConfirm');
+
+    if (!Ember.isEmpty(password) && !Ember.isEmpty(passwordConfirm)) {
+      return password === passwordConfirm;
+    }
+
+    return true;
+  }),
+
+  allowFormSubmission: Ember.computed('password', 'passowrdConfirm', 'email', 'passwordMatch', function() {
+    if (!Ember.isEmpty('password') && !Ember.isEmpty('passwordConfirm') && this.get('passwordMatch') && !Ember.isEmpty('email')) {
+      return true;
+    } else {
+      return false;
+    }
+  }),
+
+  actions: {
+    signUp: function() {
+      event.preventDefault();
+      const userParams = this.getProperties('email', 'password');
+      const user = this.store.createRecord('user', userParams);
+      this.set('isLoading', true);
+
+      return user.save().then((success) => {
+        return this.get('session').authenticate('authenticator:devise', userParams.email, userParams.password).then(() => {
+          this.set('isLoading', false);
+          this.transitionToRoute('dashboard');
+        });
+      }, (error) => {
+        this.set('serverError', error.errors);
+      });
+    },
+  }
+});
--- app/pods/user/model.js
+++ app/pods/user/model.js
@@ -4,4 +4,5 @@ export default DS.Model.extend({
     email: DS.attr('string'),
     dashboards: DS.hasMany('dashboard', { async: true }),
     defaultDashboard: DS.belongsTo('dashboard', { async: true }),
+    password: DS.attr('string'),
 });
