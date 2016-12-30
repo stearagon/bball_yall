382ae3c0f0757bbf934c8a4df613b8ace1c10f98
stearagon <me@stevearagon.io>
2016-12-29 23:50:37 -0500

    Replece user auth token on each sign out

--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -1,6 +1,16 @@
 import Devise from 'ember-simple-auth/authenticators/devise';
 import ENV from 'bball-yall/config/environment';
+import Ember from 'ember';
 
 export default Devise.extend({
   serverTokenEndpoint: `${ENV.APP.API_HOST}/users/sign_in`,
+  invalidate(data) {
+    return Ember.$.ajax({
+      url: `${ENV.APP.API_HOST}/users/sign_out`,
+      type: 'GET',
+      data: { email: data.email, token: data.token },
+    }).then(() => {
+      return this._super();
+    });
+  },
 });
--- app/authenticators/torii.js
+++ app/authenticators/torii.js
@@ -1,6 +1,19 @@
 import ToriiAuthenticator from 'ember-simple-auth/authenticators/torii';
+import ENV from 'bball-yall/config/environment';
 import Ember from 'ember';
 
 export default ToriiAuthenticator.extend({
   torii: Ember.inject.service(),
+  invalidate(data) {
+    const dataSplit = data.code.split(',');
+    const email = dataSplit[1];
+    const token = dataSplit[0];
+    return Ember.$.ajax({
+      url: `${ENV.APP.API_HOST}/users/sign_out`,
+      type: 'GET',
+      data: { email: email, token: token },
+    }).then(() => {
+      return this._super();
+    });
+  },
 });
